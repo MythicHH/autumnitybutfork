44be80aea7fa4a8e9a08078f66d17686cf393541
mythichh <112460128+MythicHH@users.noreply.github.com>
2024-12-03 15:34:25 +0800

    Update AutumnityTiers.java
    mmh stupid hardcoded repair materials

--- src/main/java/com/teamabnormals/autumnity/core/other/AutumnityTiers.java
+++ src/main/java/com/teamabnormals/autumnity/core/other/AutumnityTiers.java
@@ -8,5 +8,5 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public class AutumnityTiers {
-	public static final BlueprintArmorMaterial SNAIL_SHELL = new BlueprintArmorMaterial(new ResourceLocation(Autumnity.MOD_ID, "snail_shell"), 23, new int[]{0, 0, 5, 0}, 9, AutumnitySoundEvents.ITEM_ARMOR_EQUIP_SNAIL, 0.0F, 0.0F, () -> Ingredient.of(AutumnityItems.SNAIL_SHELL_PIECE.get()));
+	public static final BlueprintArmorMaterial SNAIL_SHELL = new BlueprintArmorMaterial(new ResourceLocation(Autumnity.MOD_ID, "snail_shell"), 23, new int[]{0, 0, 5, 0}, 9, AutumnitySoundEvents.ITEM_ARMOR_EQUIP_SNAIL, 0.0F, 0.0F, () -> Ingredient.of(NaturalistItems.SNAIL_SHELL.get()));
 }
